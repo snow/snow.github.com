7a8397a72147a15ea5b7fed616fb8799885f821a
Snow Hellsing <snow.hellsing@gmail.com>
2012-07-13 00:33:27 +0800

    CHG: tag index now present in block stream NEW: date and tag of post

--- _plugins/summarize.rb
+++ _plugins/summarize.rb
@@ -3,8 +3,8 @@ require 'logger'
 
 module Jekyll
   module Filters
-    def summarize(str, full_url, base_url='', splitstr = /\s*<!-- more -->/)
-      str.split(splitstr)[0] + %Q(<a class="hp-fullpost" href="#{base_url}#{full_url}">more</a>)
+    def summarize(str, splitstr = /\s*<!-- more -->/)
+      str.split(splitstr)[0]
     end
   end
 end
